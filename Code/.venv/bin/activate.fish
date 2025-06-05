06b9cb63bbc0db7eb0294c2ed0ae0cdcdc966b81
Jacob Lazarchik <JAL453@pitt.edu>
2025-06-04 21:31:12 -0400

    fixed issues with the .venv, rebuilt wheelhouse and requirements.lock
    just for a test.

--- Code/.venv/bin/activate.fish
+++ Code/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/framework/personal/Dutt Laboratory/Projects/servo motor /code/.venv'
+set -gx VIRTUAL_ENV /home/framework/personal/github/TDC001-Docker/Code/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
